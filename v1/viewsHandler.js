7d3175fbc83a8e07d337d193b4d88508edac9e58
MagmaBlock <823202450@qq.com>
2022-02-28 00:18:03 +0800

    修复title生成bug；新增播放时打印番剧详情

--- v1/viewsHandler.js
+++ v1/viewsHandler.js
@@ -1,6 +1,6 @@
 const { idHandler } = require("./idHandler");
 
-function viewsHandler(req, res, update) { // 处理点击量请求
+function viewsHandler(req, res, update) { // 处理播放量请求
 
     let id = idHandler(req.params[0]); // 调用函数验证 ID
     if (id == undefined) { // 如果 ID 不正常，则返回错误
@@ -9,10 +9,12 @@ function viewsHandler(req, res, update) { // 处理点击量请求
         console.log('[发送错误]', response);
     }
     else {
-        getViews(id, update).then(result => { // 增加或查询点击量
+        getViews(id, update).then(result => { // 增加或查询播放量
             let response = { code: 0, message: 'success', data: result }
             res.send(JSON.stringify(response));
-            console.log('[发送数据]', response);
+            if (update) {
+                console.log('[新增播放]', response);
+            }
         }).catch(error => {
             let response = { code: 400, message: error }
             res.send(JSON.stringify(response));
@@ -24,7 +26,7 @@ function viewsHandler(req, res, update) { // 处理点击量请求
 
 
 
-// 新增点击量
+// 新增播放量
 function getViews(id, update) {
     return new Promise((resolve, reject) => {
         // 查询数据库中是否存在该 ID 的记录
@@ -33,26 +35,26 @@ function getViews(id, update) {
         // 执行查询
         connection.query(selectSql, function (error, results) {
             if (error) throw error;
-            // console.log('[SQL返回] 目前访问: ', results);
+            let thisAnime = results
             if (results.length == 0) { // 如果数据库未查询到该 ID 的记录
                 reject('没有找到相关记录');
             }
             else { // 如果数据库查询到该 ID 的记录
-                if (update) { // 如果需要更新点击量
-                    let newViews = parseInt(results[0].views) + 1; // 新的点击量
-                    // console.log(`[更新成功] 更新为 ${newViews} !`); // 打印更新后的点击量
+                if (update) { // 如果需要更新播放量
+                    let newViews = parseInt(results[0].views) + 1; // 新的播放量
                     updateSql = `UPDATE anime SET views = '${newViews}' WHERE id = '${id}'`
                     // console.log('[SQL查询]', updateSql);
                     connection.query(updateSql, function (error, results) {
                         if (error) throw error;
                         else {
-                            // console.log('[SQL返回]', results.message);
-                            resolve(newViews) // 返回更新后的点击量
+                            console.log(`[新增播放] 新增了播放量 [${thisAnime[0].id}] ${thisAnime[0].name} => ${newViews} !`); // 打印更新后的播放量
+                            // console.log('[SQL返回]', results);
+                            resolve(newViews) // 返回更新后的播放量
                         }
                     });
                 }
                 else {
-                    resolve(parseInt(results[0].views)) // 返回点击量
+                    resolve(parseInt(results[0].views)) // 返回播放量
                 }
             }
         });
