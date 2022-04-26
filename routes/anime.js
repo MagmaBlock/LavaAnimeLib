b6aac105a7201b3fc40b05baa391a100acb40199
MagmaBlock <823202450@qq.com>
2022-04-26 21:56:11 +0800

    更新批量查询番剧API

--- routes/anime.js
+++ routes/anime.js
@@ -6,6 +6,7 @@ const getVideoList = require('../controllers/anime/getVideoList');
 const getRelations = require('../controllers/anime/getRelations');
 
 router.get(`/id/*`, getAnime.getAnimeById); // 根据id获取动画信息
+router.post(`/id`, getAnime.getAnimesByIds); // 根据id获取动画信息
 router.get(`/bgm/*`, getAnime.getAnimeByBgmID); // 根据bgmid获取动画信息
 router.get(`/list/*`, getVideoList.getVideoList); // 根据id获取动画信息
 router.get(`/relations/*`, getRelations.getRelations); // 根据id获取动画信息
