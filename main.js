3f85f04cbaa03c265c7432a35b6efca6339ba838
MagmaBlock <823202450@qq.com>
2022-05-26 21:09:03 +0800

    优化

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 // 引用库
 import express from 'express';
-// import cron from 'node-cron'; // 定时任务
 // 引用全局脚本
 import db from './common/sql.js'; // 数据库连接模块
 // 创建服务器
@@ -9,7 +8,7 @@ const app = express();
 app.use(express.json()); // 使用 Express 4 自带的 .json() , 使得全局可以使用 req.body
 app.use(express.urlencoded({ extended: true }))
 
-app.all('/*', async function (req, res, next) {
+app.all('/*', async (req, res, next) => {
     res.header('Content-Type', 'application/json'); // 指定客户端请求的属性
     res.header('Access-Control-Allow-Origin', '*'); // 允许跨域
     res.header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type');
@@ -34,6 +33,6 @@ app.use(`/v1/index`, index); // 索引页
 app.use(`/v1/zth`, zth); // Zth API，和番剧库无关
 app.use(`/v1/anime`, anime); // 动画信息
 
-const server = app.listen(8090, function () {
+const server = app.listen(8090, () => {
     console.log("[启动信息] 服务器已启动, 访问端口为: " + server.address().port)
 })
