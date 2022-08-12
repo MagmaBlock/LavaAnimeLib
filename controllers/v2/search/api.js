c22b25d7ccea1e02eebaf8a4d5dbda185d82d366
岩浆块Magma <magmablock@qq.com>
2022-08-12 20:40:34 +0800

    支持未搜索时预搜索的 API，支持以预搜索的词优先排序

--- controllers/v2/search/api.js
+++ controllers/v2/search/api.js
@@ -1,6 +1,6 @@
 import serverError from "../error/serverError.js"
 import wrongQuery from "../error/wrongQuery.js"
-import { searchAnimes } from "./search.js"
+import { quickSearch, searchAnimes } from "./search.js"
 
 export async function searchAnimesAPI(req, res) {
     let value = req.query.value
@@ -8,6 +8,19 @@ export async function searchAnimesAPI(req, res) {
 
     try {
         let searchResults = await searchAnimes(value)
+        res.send({ code: 200, message: '成功', data: searchResults })
+    } catch (error) {
+        console.error(error);
+        return serverError(res)
+    }
+}
+
+export async function quickSearchAPI(req, res) {
+    let value = req.query.value
+    if (!value || typeof value !== 'string') return wrongQuery(res)
+
+    try {
+        let searchResults = await quickSearch(value)
         res.send({ code: 200, message: '成功', data: searchResults })
     } catch (error) {
         console.error(error);
--- routes/v2/search.js
+++ routes/v2/search.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import { searchAnimesAPI } from '../../controllers/v2/search/api.js';
+import { quickSearchAPI, searchAnimesAPI } from '../../controllers/v2/search/api.js';
 const router = Router();
 
 
 router.get(`/`, searchAnimesAPI); // 搜索
+router.get('/quick', quickSearchAPI) // 预搜索关键词显示
 
 export default router;
