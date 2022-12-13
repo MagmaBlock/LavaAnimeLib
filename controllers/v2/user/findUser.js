d520fcc7298b565b45e5a8133794d936dd411882
岩浆块Magma <magmablock@qq.com>
2022-12-13 21:49:43 +0800

    查找用户支持缓存

--- controllers/v2/user/findUser.js
+++ controllers/v2/user/findUser.js
@@ -1,7 +1,12 @@
+import cache from "../../../common/cache.js"
 import { promiseDB } from "../../../common/sql.js"
 
 export async function findUserByID(userID) {
   if (!userID) return false
+
+  if (cache.user?.[userID]) {
+    return dbUserParser(cache.user[userID])
+  }
 
   let findReult = await promiseDB.query(
     'SELECT * FROM user WHERE id = ?',
@@ -9,8 +14,22 @@ export async function findUserByID(userID) {
   )
   findReult = findReult[0]
   if (findReult[0]) {
-    return findReult[0]
+    // cache
+    if (!cache.user) cache.user = {}
+    cache.user[userID] = findReult[0]
+    cache.user[userID].expirationTime = new Date().getTime() + 1000 * 60 * 5 // 五分钟后过期
+    return dbUserParser(findReult[0])
   } else {
     return false
   }
+}
+
+function dbUserParser(userData) {
+  try {
+    userData.data = JSON.parse(userData.data)
+    userData.settings = JSON.parse(userData.settings)
+  } catch (error) {
+    return userData
+  }
+  return userData
 }
