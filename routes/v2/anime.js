cac418db4e13b421f914dc500defafe1509a3162
岩浆块Magma <magmablock@qq.com>
2022-12-23 18:29:50 +0800

    增加全局鉴权中间件；动画文件获取和搜索API增加登录鉴权

--- routes/v2/anime.js
+++ routes/v2/anime.js
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 import { addAnimeViewAPI, getAnimeByIDAPI, getAnimesByIDAPI, getAnimeViewAPI, getFilesByIDAPI } from '../../controllers/v2/anime/api.js';
+import { loginRequire } from '../../controllers/v2/globalAuth/auth.js';
 const router = Router();
 
 router.get(`/get`, getAnimeByIDAPI);
 router.post(`/get`, getAnimesByIDAPI);
-router.get('/file', getFilesByIDAPI);
+
+router.get('/file', [loginRequire, getFilesByIDAPI]); // 使用多个中间件
+
 router.get('/view/get', getAnimeViewAPI);
-router.post('/view/add', addAnimeViewAPI);
+router.post('/view/add', [loginRequire, addAnimeViewAPI]);
 
 export default router;
