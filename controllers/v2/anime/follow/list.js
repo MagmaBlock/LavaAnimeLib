b7220e411490597c81284935ba85d1803b3318b5
岩浆块Magma <magmablock@qq.com>
2023-02-07 01:03:32 +0800

    支持查询我的追番

---
+++ controllers/v2/anime/follow/list.js
@@ -0,0 +1,44 @@
+import { promiseDB } from "../../../../common/sql.js"
+import success from "../../response/2xx/success.js"
+import wrongQuery from "../../response/4xx/wrongQuery.js"
+import serverError from "../../response/5xx/serverError.js"
+import { getAnimeByID } from "../get.js"
+
+// 获取追番列表
+// /v2/anime/follow/list
+export async function getAnimeFollowListAPI(req, res) {
+  let { status, page, pageSize } = req.body
+  if (status == undefined || !status.length || page < 1 || pageSize < 1 || pageSize > 200) {
+    return wrongQuery(res)
+  }
+  if (!page) { // page 从 1 开始 
+    page = 1
+  }
+  if (!pageSize) {
+    pageSize = 15
+  }
+
+
+  let allRawResult = []
+  for (let oneStatus of status) {
+    try {
+      let result = await promiseDB.query(
+        "SELECT * FROM follow WHERE user_id = ? AND status = ? ORDER BY edit_time DESC, anime_id DESC LIMIT ?,?",
+        [req.user.id, oneStatus, 0 + pageSize * (page - 1), pageSize]
+      )
+      for (let followRecord of result[0]) {
+        console.log(followRecord.anime_id);
+        allRawResult.push({
+          status: followRecord.status,
+          editTime: followRecord.edit_time.getTime(),
+          anime: await getAnimeByID(followRecord.anime_id)
+        })
+      }
+    } catch (error) {
+      console.error(error);
+      return serverError(res)
+    }
+  }
+
+  success(res, allRawResult)
+}
--- routes/v2/anime.js
+++ routes/v2/anime.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { addAnimeViewAPI, getAnimeByIDAPI, getAnimesByIDAPI, getAnimeViewAPI, getFilesByIDAPI } from '../../controllers/v2/anime/api.js';
+import { getAnimeFollowListAPI } from '../../controllers/v2/anime/follow/list.js';
 import { loginRequire } from '../../controllers/v2/globalAuth/auth.js';
 const router = Router();
 
@@ -10,5 +11,7 @@ router.get('/file', [loginRequire, getFilesByIDAPI]); // 使用多个中间件
 
 router.get('/view/get', getAnimeViewAPI);
 router.post('/view/add', [loginRequire, addAnimeViewAPI]);
+
+router.post('/follow/list', [loginRequire, getAnimeFollowListAPI])
 
 export default router;
