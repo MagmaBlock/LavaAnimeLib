5620bf2bd6fdda9421ab80f3eaf7830421045895
岩浆块Magma <magmablock@qq.com>
2023-04-17 14:46:03 +0800

    修复 节点配置中即使自定义了 Alist 节点 Host，获取时仍然使用主 Host 的BUG

--- controllers/v2/anime/file.js
+++ controllers/v2/anime/file.js
@@ -1,58 +1,83 @@
-import { AlistAPI } from "../../../common/api.js"
-import config from "../../../common/config.js"
-import { getDrivePath } from "../drive/main.js"
-import { getAnimeByID } from "./get.js"
-import { parseFileName } from "./tag.js"
+import axios from "axios";
+import config from "../../../common/config.js";
+import { getDriveHost, getDrivePath } from "../drive/main.js";
+import { getAnimeByID } from "./get.js";
+import { parseFileName } from "./tag.js";
 
 export async function getFilesByID(laID, drive) {
-    // 根据 ID 获取某番剧目录下的文件和文件夹名
+  // 根据 ID 获取某番剧目录下的文件和文件夹名
 
-    let anime = await getAnimeByID(laID)
-    if (anime.deleted) return false // 404
+  let anime = await getAnimeByID(laID);
+  if (anime.deleted) return false; // 404
 
-    let drivePath = getDrivePath(drive)
+  let drivePath = getDrivePath(drive);
+  let driveHost = getDriveHost(drive);
 
-    let alistAPIResult = (await AlistAPI.post('/api/fs/list', {
-        path: drivePath + '/' + anime.index.year + '/' + anime.index.type + '/' + anime.index.name
-    })).data
+  let alistAPIResult = (
+    await axios.post(
+      "/api/fs/list",
+      {
+        path:
+          drivePath +
+          "/" +
+          anime.index.year +
+          "/" +
+          anime.index.type +
+          "/" +
+          anime.index.name,
+      },
+      {
+        baseURL: driveHost,
+      }
+    )
+  ).data;
 
-    if (alistAPIResult.code == 200) {
-        let thisDir = new Array() // 存储解析后的文件列表结果
-        let files = alistAPIResult.data.content
-        for (let i in files) {
-            let thisFile = files[i]
-            let thisFileInfo = { // 当前文件的信息
-                name: thisFile.name,
-                size: thisFile.size,
-                updated: thisFile.modified,
-                driver: alistAPIResult.data.provider,
-                thumbnail: thisFile.thumb,
-            }
-            if (thisFile.is_dir == true) { // 文件夹处理
-                thisDir.push({
-                    ...thisFileInfo,
-                    type: 'dir'
-                })
-            } else { // 普通文件
-                let fileUrl = config.alist.host
-                    + '/d' + drivePath
-                    + '/' + encodeURIComponent(anime.index.year)
-                    + '/' + encodeURIComponent(anime.index.type)
-                    + '/' + encodeURIComponent(anime.index.name)
-                    + '/' + encodeURIComponent(thisFile.name)
-                    + '?sign=' + thisFile.sign
-                thisDir.push({
-                    ...thisFileInfo,
-                    ...parseFileName(thisFile.name),
-                    url: fileUrl,
-                    tempUrl: fileUrl,
-                    type: 'file'
-                })
-            }
-        }
-        return thisDir
-    } else {
-        return false
+  if (alistAPIResult.code == 200) {
+    let thisDir = new Array(); // 存储解析后的文件列表结果
+    let files = alistAPIResult.data.content;
+    for (let i in files) {
+      let thisFile = files[i];
+      let thisFileInfo = {
+        // 当前文件的信息
+        name: thisFile.name,
+        size: thisFile.size,
+        updated: thisFile.modified,
+        driver: alistAPIResult.data.provider,
+        thumbnail: thisFile.thumb,
+      };
+      if (thisFile.is_dir == true) {
+        // 文件夹处理
+        thisDir.push({
+          ...thisFileInfo,
+          type: "dir",
+        });
+      } else {
+        // 普通文件
+        let fileUrl =
+          config.alist.host +
+          "/d" +
+          drivePath +
+          "/" +
+          encodeURIComponent(anime.index.year) +
+          "/" +
+          encodeURIComponent(anime.index.type) +
+          "/" +
+          encodeURIComponent(anime.index.name) +
+          "/" +
+          encodeURIComponent(thisFile.name) +
+          "?sign=" +
+          thisFile.sign;
+        thisDir.push({
+          ...thisFileInfo,
+          ...parseFileName(thisFile.name),
+          url: fileUrl,
+          tempUrl: fileUrl,
+          type: "file",
+        });
+      }
     }
-
+    return thisDir;
+  } else {
+    return false;
+  }
 }
--- controllers/v2/drive/main.js
+++ controllers/v2/drive/main.js
@@ -3,20 +3,33 @@ import config from "../../../common/config.js";
 export function getDriveList() {
   return {
     default: config.drive.default,
-    list: config.drive.list.map(drive => {
+    list: config.drive.list.map((drive) => {
       return {
-        id: drive.id, name: drive.name
-      }
-    })
-  }
+        id: drive.id,
+        name: drive.name,
+      };
+    }),
+  };
 }
 
 // 获取指定的 Drive 的路径，如 /2AG_CF/LavaAnimeLib
 export function getDrivePath(drive) {
-  let driveID = drive ? drive : config.drive.default
-  let drivePath
-  config.drive.list.forEach(drive => {
-    if (drive.id == driveID) drivePath = drive.path
-  })
-  return drivePath
+  let driveID = drive ? drive : config.drive.default;
+  let drivePath;
+  config.drive.list.forEach((drive) => {
+    if (drive.id == driveID) drivePath = drive.path;
+  });
+  return drivePath;
+}
+
+/**
+ * 获取指定的 Drive 的 host，如 https://share.xxx.com
+ */
+export function getDriveHost(drive) {
+  let driveID = drive ? drive : config.drive.default;
+  let driveHost;
+  config.drive.list.forEach((drive) => {
+    if (drive.id == driveID) driveHost = drive.host;
+  });
+  return driveHost;
 }
