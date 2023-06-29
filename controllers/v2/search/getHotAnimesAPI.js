228fec7ef544735ee20670dc05ab98fced4d0aa2
岩浆块Magma <magmablock@qq.com>
2023-06-29 23:11:38 +0800

    修复 升级新播放历史后，“大家在看”功能不可用

--- controllers/v2/search/getHotAnimesAPI.js
+++ controllers/v2/search/getHotAnimesAPI.js
@@ -1,28 +1,16 @@
 import cache from "../../../common/cache.js";
 import { promiseDB } from "../../../common/sql.js";
 import { getAnimeByID } from "../anime/get.js";
+import { animeParser } from "../parser/animeParser.js";
 import success from "../response/2xx/success.js";
 
 // 获取近期热门 (view 增量最大前十)
 export async function getHotAnimesAPI(req, res) {
-  let daysBefore = new Date().getTime() - 1000 * 60 * 60 * 24 * 7;
-  daysBefore = new Date(daysBefore);
-
   let queryResult = await promiseDB.query(
-    `
-    SELECT id, count(*) FROM \`views\` 
-    WHERE time > ? GROUP BY id ORDER BY count(*) DESC`,
-    [daysBefore]
+    "SELECT vh.animeID, COUNT(*) watchCount, a.* FROM `view_history` vh JOIN anime a ON vh.animeID = a.id WHERE DATEDIFF(NOW(), vh.lastReportTime) <= 7 GROUP BY animeID ORDER BY watchCount DESC LIMIT 10"
   );
-  let hotIDs = []; // 前十的 laID 数组
-  queryResult[0].forEach((hot, index) => {
-    if (index < 10) hotIDs.push(hot.id);
-  });
-  let hot = []; // 相应的 AnimeData
-  for (let laID of hotIDs) {
-    let thisAnimeData = await getAnimeByID(laID);
-    if (!thisAnimeData?.deleted) hot.push(thisAnimeData);
-  }
 
-  success(res, hot);
+  let result = await animeParser(queryResult[0]);
+
+  success(res, result);
 }
