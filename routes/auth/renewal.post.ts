116aee21772e62983ff718f50c1b4c5a67ae7af5
岩浆块Magma <magmablock@qq.com>
2023-12-25 23:05:48 +0800

    完成 token 刷新 API

--- routes/auth/renewal.post.ts
+++ routes/auth/renewal.post.ts
@@ -1,15 +1,7 @@
-import { ClientError, UnauthorizedError } from "../../src/class/error/error";
 import { tokenRenewal } from "../../src/managers/token/renewal";
 
 export default eventHandler(async (event) => {
   const { token } = await readBody(event);
 
-  try {
-    return tokenRenewal(token);
-  } catch (error) {
-    if (error instanceof ClientError) {
-      setResponseStatus(event, error.getStatusCode());
-      return { message: error.message };
-    }
-  }
+  return { token: tokenRenewal(token) };
 });
