8d8fd80cfc8818e8972a7634bc5579ed57c5d886
岩浆块Magma <magmablock@qq.com>
2023-12-25 23:22:06 +0800

    新增 动画创建 API

--- routes/admin/anime/create.post.ts
+++ routes/admin/anime/create.post.ts
@@ -1,28 +1,17 @@
-import { Prisma } from "@prisma/client";
+import { animeCreate } from "../../../src/managers/anime/create";
 
 export default defineEventHandler(async (event) => {
-  const user = await readAdmin(event);
+  await readAdmin(event);
   const { name, originalName, summary, date, platform, nsfw, bdrip } =
     await readBody(event);
 
-  try {
-    const create = await usePrisma.anime.create({
-      data: {
-        name,
-        originalName,
-        summary,
-        date,
-        platform,
-        nsfw,
-        bdrip,
-      },
-    });
-
-    return create;
-  } catch (error) {
-    if (error instanceof Prisma.PrismaClientKnownRequestError) {
-      // TODO
-    }
-    throw error;
-  }
+  return await animeCreate(
+    name,
+    originalName,
+    summary,
+    date,
+    platform,
+    nsfw,
+    bdrip
+  );
 });
