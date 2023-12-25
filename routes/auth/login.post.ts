4019e0cab05d44c7483683d33e97c7275cec1726
岩浆块Magma <magmablock@qq.com>
2023-12-25 23:04:05 +0800

    优化 不再由 API 层进行错误处理

--- routes/auth/login.post.ts
+++ routes/auth/login.post.ts
@@ -1,21 +1,11 @@
-import { ClientError } from "../../src/class/error/error";
 import { userLogin } from "../../src/managers/user/login";
 
 export default eventHandler(async (event) => {
   const { account, password } = await readBody(event);
 
-  try {
-    const login = await userLogin(account, password);
+  const login = await userLogin(account, password);
+  login.user.encryption = undefined;
+  login.user.password = undefined;
 
-    login.user.encryption = undefined;
-    login.user.password = undefined;
-
-    return login;
-  } catch (error) {
-    if (error instanceof ClientError) {
-      setResponseStatus(event, error.getStatusCode());
-      return { message: error.message };
-    }
-    throw error;
-  }
+  return login;
 });
