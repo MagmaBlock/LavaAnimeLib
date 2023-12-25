1d8b101c7253278975340b23536e64761c98a3c1
岩浆块Magma <magmablock@qq.com>
2023-12-25 23:05:30 +0800

    新增 注册 API

--- routes/auth/register.post.ts
+++ routes/auth/register.post.ts
@@ -1,16 +1,15 @@
-import { ClientError } from "../../src/class/error/error";
 import { userCreate } from "../../src/managers/user/create";
 
 export default eventHandler(async (event) => {
   let { email, name, password, inviteCode } = await readBody(event);
 
-  try {
-    return await userCreate(email, name, password, inviteCode);
-  } catch (error) {
-    if (error instanceof ClientError) {
-      setResponseStatus(event, error.getStatusCode());
-      return { message: error.message, cause: error.cause };
-    }
-    throw error;
-  }
+  const register = await userCreate(email, name, password, inviteCode);
+  register.encryption = undefined;
+  register.password = undefined;
+
+  const token = useAuth.sign({
+    id: register.id,
+  });
+
+  return { token, user: register };
 });
